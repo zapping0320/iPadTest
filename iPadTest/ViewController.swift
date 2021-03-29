81d864478e8700fa84b4a7211df596e57c83055d
john donghyun kim <zapping0320@gmail.com>
2021-03-29 14:08:37 +0900

    Initial Commit

---
+++ iPadTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPadTest
+//
+//  Created by DD Dev on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
