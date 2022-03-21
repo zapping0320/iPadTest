05a66c5b6383c670fe701fe6cfc5d70f56f97b55
John DongHyun Kim <zapping0320@gmail.com>
2022-03-21 13:18:39 +0900

    [aes-256-cbc test] 키가 없어서 일단 hold

--- iPadTest/ViewController.swift
+++ iPadTest/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        currentText.text = "크립토택스1234"
     }
 
 
+    @IBAction func encrytAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func decryptAction(_ sender: Any) {
+        
+    }
 }
 
