e204e38df0b73d9e67227de9ce40466f3727ccdc
John DongHyun Kim <zapping0320@gmail.com>
2022-03-29 12:52:40 +0900

    업데이트 확인시 업데이트를 시도할 경우 관련 작업을 하고 앱을 강제 종료 시키는 테스트 코드 추가
    ref https://zeddios.tistory.com/1252

--- iPadTest/ViewController.swift
+++ iPadTest/ViewController.swift
@@ -37,5 +37,28 @@ class ViewController: UIViewController {
     @IBAction func decryptAction(_ sender: Any) {
         
     }
+    
+    @IBAction func forceQuitorNotAction(_ sender: Any) {
+        let alert = UIAlertController(title: title,
+                                      message: "업데이트하시겠습니까?",
+                                      preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인",
+                                          style: .default, handler: {result in
+            print("업데이트")
+            UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { exit(0)  }
+            
+        })
+        
+        alert.addAction(confirmAction)
+        
+        let cancelAction = UIAlertAction(title: "취소",
+                                         style: .default, handler: {result in
+            print("취소")
+        })
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
